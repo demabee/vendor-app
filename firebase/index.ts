1bd6f436917f858f4dba87344dfd711d5e86ddb8
Andre Demavivas <andredemavivas@gmail.com>
2024-06-11 19:25:57 +0800

    secure firebase credentials

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,25 +1,32 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCkePGxMHhHb8NML5RL1XQfjBYSuBnnxB8",
-  authDomain: "vendor-app-e8ed9.firebaseapp.com",
-  projectId: "vendor-app-e8ed9",
-  storageBucket: "vendor-app-e8ed9.appspot.com",
-  messagingSenderId: "374089036642",
-  appId: "1:374089036642:web:994c0bd4cf1219ecde323b",
-  measurementId: "G-7RFM3CQEB7"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+// const auth = getAuth(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
 const firestore = getFirestore(app);
+
 
 export { auth, firestore };
